d32d29545a9c16547e4de409d6762cf2ded2e2f3
Nikola325 <nanikolov24a@gmail.com>
2025-12-18 10:49:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.title("First project")
-name = st.text_input("enter name:")
+name = st.text_input("enter name: ")
 if name:
-  st.write(f"hello{name}")
+  st.write(f"hello {name}")
+
+age = st.text_input("enter age: ")
+if age:
+  st.write(f"your age is {age}")
